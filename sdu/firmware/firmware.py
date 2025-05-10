fa3b17c27d2c37020f39e35d9765454d773bd473
Koji Hirai <17133941+kojihirai@users.noreply.github.com>
2025-05-11 00:03:24 +0900

    sdu

--- sdu/firmware/firmware.py
+++ sdu/firmware/firmware.py
@@ -84,11 +84,14 @@ class SensorController:
 
                 # Convert voltage to current
                 if sensor_name == "DRILL":
-                    current = voltage / 0.020  # 20 mV/A
+                    current = voltage
+                    # current = voltage / 0.020  # 20 mV/A
                 elif sensor_name == "POWER":
-                    current = voltage / 0.100  # 100 mV/A
+                    current = voltage
+                    # current = voltage / 0.100  # 100 mV/A
                 elif sensor_name == "LINEAR":
-                    current = voltage / 0.1875  # 187.5 mV/A
+                    current = voltage
+                    # current = voltage / 0.1875  # 187.5 mV/A
                 elif sensor_name == "VIN":
                     current = signed * REF * VOLTAGE_SCALE / 0x7FFFFFFF
 
