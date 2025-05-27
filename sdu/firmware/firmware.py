7d6580da7278bf80b96b47a836bd5eef7269a776
Koji Hirai <17133941+kojihirai@users.noreply.github.com>
2025-05-28 01:59:52 +0900

    test

--- sdu/firmware/firmware.py
+++ sdu/firmware/firmware.py
@@ -76,13 +76,13 @@ class SensorController:
 
                 if sensor_name == "DRILL":
                     # current = voltage
-                    current = voltage / 110
+                    current = voltage / 5
                 elif sensor_name == "POWER":
                     # current = voltage
                     current = voltage / 5
                 elif sensor_name == "LINEAR":
                     # current = voltage
-                    current = voltage / 292.5
+                    current = voltage / 5
 
                 measurements[sensor_name] = current
 
