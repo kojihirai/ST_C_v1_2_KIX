f07825c84b3722eec4976df569999bc92b501208
Koji Hirai <17133941+kojihirai@users.noreply.github.com>
2025-06-07 22:54:12 -0700

    test measure

--- sdu/firmware/firmware.py
+++ sdu/firmware/firmware.py
@@ -25,9 +25,9 @@ ADC_CHANNELS = {
 # Power In:     100 mV/A → 0.1 V/A
 # Linear:       187.5 mV/A → 0.1875 V/A
 CONVERSION = {
-    "DRILL": 0.1,
-    "POWER": 1,
-    "LINEAR": 0.19125
+    "DRILL": 0.02,
+    "POWER": 0.1,
+    "LINEAR": 0.1875
 }
 
 class SensorController:
