1412ad6a3297710e5fab6ee152667db34a813206
Koji Hirai <17133941+kojihirai@users.noreply.github.com>
2025-06-09 03:25:33 -0700

    new load cell

--- lcu/firmware/firmware.py
+++ lcu/firmware/firmware.py
@@ -415,6 +415,7 @@ class MotorSystem:
             }
             self.logger.log(data)
             self.client.publish(f"{DEVICE_ID}/data", json.dumps(data))
+            print(f"Published data: {data}") 
             time.sleep(0.2)
 
     def stop(self):
