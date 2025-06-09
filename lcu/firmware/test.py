4f736a13c0a4cd820a9be158b2dff6aea4ad0ec1
Koji Hirai <17133941+kojihirai@users.noreply.github.com>
2025-06-09 03:34:32 -0700

    new load cell

--- lcu/firmware/test.py
+++ lcu/firmware/test.py
@@ -52,7 +52,7 @@ class LoadCellDriver:
     def read_parameter(self, address, length=1, signed=False):
         try:
             # Read holding registers
-            response = self.client.read_holding_registers(address, length, slave=self.slave_id)
+            response = self.client.read_holding_registers(address, length)
             if not response.isError():
                 if length == 1:
                     # 16-bit register
