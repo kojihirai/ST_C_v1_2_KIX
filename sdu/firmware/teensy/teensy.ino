e720d1cb16ab18a49017461284b384a2a0c9e532
Koji Hirai <koji@duneindustries.co>
2025-06-14 10:58:41 +0900

    teensy updated

--- sdu/firmware/teensy/teensy.ino
+++ sdu/firmware/teensy/teensy.ino
@@ -1,7 +1,12 @@
-
 const int DRILL_CURRENT_PIN  = A6;
 const int POWER_CURRENT_PIN  = A7;
 const int LINEAR_CURRENT_PIN = A8;
+
+const float VREF         = 3.3f;
+const int   ADC_MAX      = 4095;
+const float SENS_DRILL   = 0.1f; 
+const float SENS_POWER   = 0.1f;
+const float SENS_LINEAR  = 0.1875f;
 
 void setup() {
   Serial.begin(115200);
@@ -11,14 +16,22 @@ void setup() {
 }
 
 void loop() {
-  int drill  = analogRead(DRILL_CURRENT_PIN);
-  int power  = analogRead(POWER_CURRENT_PIN);
-  int linear = analogRead(LINEAR_CURRENT_PIN);
+  uint16_t rawDrill  = analogRead(DRILL_CURRENT_PIN);
+  uint16_t rawPower  = analogRead(POWER_CURRENT_PIN);
+  uint16_t rawLinear = analogRead(LINEAR_CURRENT_PIN);
 
-  Serial.print(drill);
+  float voltDrill  = rawDrill  * VREF / ADC_MAX;
+  float voltPower  = rawPower  * VREF / ADC_MAX;
+  float voltLinear = rawLinear * VREF / ADC_MAX;
+
+  float currDrill  = (voltDrill  * 1000.0f) / SENS_DRILL;
+  float currPower  = (voltPower  * 1000.0f) / SENS_POWER;
+  float currLinear = (voltLinear * 1000.0f) / SENS_LINEAR;
+
+  Serial.print(currDrill,  2);
   Serial.print(',');
-  Serial.print(power);
+  Serial.print(currPower,  2);
   Serial.print(',');
-  Serial.print(linear);
+  Serial.print(currLinear, 2);
   Serial.print('\n');
 }
